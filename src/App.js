fcae9ec3d3a7c3490adaed28ca4642d2a4446f93
lse1656 <lse1656@naver.com>
2021-07-23 15:02:38 +0900

    🔧 Istall prop-types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import PropType from 'prop-types';
+
+
+const foodILike = [
+  {
+    id:1,
+    name:"Kimchi",
+    rating: 3
+  },
+  {
+    id:2,
+    name:"Bibimbap",
+    rating: 5
+  },
+  {
+    id:3,
+    name:"Samgyeopsal",
+    rating: 4.8
+  }
+]
+
+function Food({ name, rating }){
+  return (
+  <>
+    <h2>I like {name}</h2>
+    <p>{rating} / 5.0</p>
+  </>
+  )
+}
+
+Food.PropType = {
+  ba
+}
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello !!!!!!!!</div>
+    <div>
+      { foodILike.map(dish => {
+        return <Food key={dish.id} name={dish.name} rating={dish.rating}/>
+      }) }
     </div>
   );
 }
+
 
 export default App;
